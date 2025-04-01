a3cdacfda7c60923ee65047184278ace00182997
Luis <savl021227@gmail.com>
2025-04-01 16:17:17 -0600

    Segundo commit: creacion de tabla con MUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,59 @@
-import { useState } from 'react'
+import { useState, useEffect, Component } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper} from "@mui/material"
 
 function App() {
   const [count, setCount] = useState(0)
+  useEffect(()=>{
+    if(count>10){
+      setCount(10)
+      alert("Alcanzaste el limite del contador")
+    }
+  },[count])
+
+  const usersList = [
+    {
+      name: "Saul", 
+      lastName: "Valenzuela",
+      age: "15"
+    },
+    {
+      name: "Antonio", 
+      lastName: "Medina",
+      age: "17"
+    },
+    {
+      name: "Jesus", 
+      lastName: "Valverde",
+      age: "22"
+    }
+  ]
+
+  return(
+    <TableContainer component={Paper}>
+    <Table>
+    <TableHead>
+    <TableRow>
+      <TableCell>Nombre</TableCell>
+      <TableCell>Apellido</TableCell>
+      <TableCell>Edad</TableCell>
+    </TableRow>
+    </TableHead>
+    <TableBody>
+      {usersList.map((user, index) => (
+        <TableRow key={index}>
+          <TableCell>{user.name}</TableCell>
+          <TableCell>{user.lastName}</TableCell>
+          <TableCell>{user.age}</TableCell>
+        </TableRow>
+      ))}
+    </TableBody>
+    </Table>
+    </TableContainer>
+  )
+  
 
   return (
     <>
